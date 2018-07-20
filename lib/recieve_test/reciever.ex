291e9458e467d6871a7fab42fe619ba26a75177b
Oleg Tarasenko <oltarasenko@gmail.com>
2018-07-20 23:13:28 +0200

    Some changes to the test

--- lib/recieve_test/reciever.ex
+++ lib/recieve_test/reciever.ex
@@ -1,62 +1,51 @@
 defmodule Reciever do
-  def loop1() do
+  def loop() do
     receive do
       "scnenario1" <> _rest -> :ok
       "scnenario2" <> _rest -> :ok
       "scnenario3" <> _rest -> :ok
       "scnenario4" <> _rest -> :ok
-      _ -> :not_found
+      _ -> :ok
     end
 
-    # IO.puts("Resp #{inspect(resp)}")
-    :timer.sleep(1000)
-    loop1()
+    {_, len} = Process.info(self(), :message_queue_len)
+    send(:stats, len)
+    loop()
   end
 
   def test1() do
-    pid = spawn(&loop1/0)
-
-    # messages =
-    #   [
-    #     Enum.map(1..1000, fn _ -> "scnenario1_" <> EntropyString.random() end),
-    #     Enum.map(1..1000, fn _ -> "scnenario2_" <> EntropyString.random() end),
-    #     Enum.map(1..1000, fn _ -> "scnenario3_" <> EntropyString.random() end),
-    #     Enum.map(1..1000, fn _ -> "scnenario4_" <> EntropyString.random() end)
-    #   ]
-    #   |> List.flatten()
-
-    # Kernel.spawn(fn ->
-    #   Task.async_stream(1..10_000, fn _ ->
-    #     send(pid, "scnenario4_some_binary")
-    #     :timer.sleep(1_000)
-    #   end)
-    #   |> Enum.map(fn x -> x end)
-    # end)
-
-    # IO.puts("Stream sent")
-
-    # Task.async_stream(1..1000, fn -> send(pid, "scnenario1_" <> EntropyString.random()) end, :max_concurrency: 30)
-    # Task.async_stream(1..1000, fn -> send(pid, "scnenario1_" <> EntropyString.random()) end, :max_concurrency: 30)
-    # Task.async_stream(1..1000, fn -> send(pid, "scnenario1_" <> EntropyString.random()) end, :max_concurrency: 30)
-    # Enum.each(messages, fn msg -> send(pid, msg) end)
-    send_message(pid, "scnenario1", 100_000, 100)
-    send_message(pid, "scnenario1", 100_0000, 10000)
-
-    Enum.map(
-      1..200,
-      fn _ ->
-        process_info = :erlang.process_info(pid, :message_queue_len)
-        IO.puts("#{inspect(process_info)}")
-        :timer.sleep(1000)
-      end
-    )
+    loop_pid = spawn(&Reciever.loop/0)
+    Process.register(spawn(fn -> stats(50_000) end), :stats)
+    send_messages(loop_pid, "scnenario1_test", 50000)
   end
 
-  defp send_message(pid, msg, total_messages, delay) do
-    Task.async_stream(1..total_messages, fn _ ->
-      send(pid, msg <> "_some_binary")
-      :timer.sleep(delay)
-    end)
+  defp send_messages(pid, msg, total_messages) do
+    Task.async_stream(
+      1..total_messages,
+      fn _ ->
+        send(pid, msg <> "_some_binary")
+      end,
+      max_concurrency: 500
+    )
     |> Enum.map(fn x -> x end)
+
+    IO.puts("Sent all...")
+  end
+
+  def stats(num) do
+    ts1 = Time.utc_now()
+    {:ok, fd} = File.open("/tmp/bench", [:delayed_write, :append])
+
+    details =
+      Enum.map(1..num, fn _ ->
+        receive do
+          length -> IO.puts(fd, "#{length}")
+        end
+      end)
+
+    File.close(fd)
+    ts2 = Time.utc_now()
+    diff = Time.diff(ts2, ts1, :microsecond)
+    IO.puts("Time spent #{diff}")
   end
 end
